e522e0d1db2fc7fcb14dfe4caa3863fc0de45cb9
Dang khac Dat <khacdatdang@gmail.com>
2021-12-10 03:42:40 +0000

    Add book review

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,13 @@ class BooksController < ApplicationController
   
   # GET /books or /books.json
   def index
+      
     @books = Book.all
   end
 
   # GET /books/1 or /books/1.json
   def show
+      @book_review = BookReview.new
   end
 
   # GET /books/new
